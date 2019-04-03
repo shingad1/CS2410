344629ba628905eea79f87a1befca660c40076fe
Devin S <devin_shingadia11@hotmail.com>
2019-04-04 00:36:27 +0100

    update

---
+++ larabank/app/Account.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model; //its an eloquent model as it extends it
+
+class Account extends Model
+{
+    //
+}
---
+++ larabank/app/Http/Controllers/AccountController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Account;
+
+class AccountController extends Controller
+{
+    public function show($id) {
+      $account = Account::find($id);
+      return view('show', array('account' => $account));
+    }
+
+    public function list() {
+      return view ('list', array('accounts'=>Account::all()));
+    }
+}
