64682d5b97946b8d4325a1f20eb6591ef3a1556a
Devin S <devin_shingadia11@hotmail.com>
2019-04-09 12:13:28 +0100

    latest

--- animalsanctuary/resources/views/show.blade.php
+++ animalsanctuary/resources/views/show.blade.php
@@ -4,8 +4,9 @@
     <title> Animal: {{ $animal->Name }} </title>
   </head>
   <body>
-    <h1> Animal ID: {{ $animal->id }} </h1>
+    <h1> Animal Name: {{ $animal-> name }} </h1>
     <ul>
+      <li> Animal ID: {{ $animal-> id }} </li>
       <li> Animal Description: {{ $animal->description }} </li>
       <li> Animal DOB: {{ $animal-> DOB }} </li>
       <li> Animal Availability: {{ $animal-> availability }} </li>
--- animalsanctuary/routes/web.php
+++ animalsanctuary/routes/web.php
@@ -11,5 +11,12 @@
 |
 */
 
-Route::get('list', 'AnimalController@listAll'); //may need to change list here to listAll
+//List all of the animals
+Route::get('list', 'AnimalController@listAll');
+//List a specific animal, given it's ID (URL)
 Route::get('show/{id}', 'AnimalController@show');
+
+//User authentication routes
+Auth::routes();
+//Assign the home url to the home controller
+Route::get('/home', 'HomeController@index')->name('home');
